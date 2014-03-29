e2f9b7996ce9dd89db493b99a873c0b2301f2b25
antoniocuga <antoniocuga@gmail.com>
2014-03-29 11:11:11 -0500

    Añadiendo ejercicio de Lista de noticias

--- module_3/Clase2/js/news.js
+++ module_3/Clase2/js/news.js
@@ -10,17 +10,24 @@ function loadHTML() {
         var titulo = document.createElement("h2");
         var parrafo = document.createElement("p");
         var link = document.createElement("a");
+        var img = document.createElement("img");
+        var span = document.createElement("span");
 
         titulo.innerHTML = datanews[index][0];
         parrafo.innerHTML = datanews[index][1];
+        img.setAttribute("src", datanews[index][3]);
         link.innerHTML = "Leer mas";
         link.setAttribute("href", "#");
         link.setAttribute("onclick", "getNews(" + index + ")");
+        span.innerHTML = datanews[index][2];
+        span.setAttribute("id", "news_" + index);
 
 
         item.appendChild(titulo);
         item.appendChild(parrafo);
         item.appendChild(link);
+        item.appendChild(img);
+        item.appendChild(span);
 
         lista.appendChild(item);
 
@@ -32,10 +39,23 @@ function loadHTML() {
 
 function getNews(index) {
 
+    //obteniendo el contenedor del overlay
     var content = document.getElementsByClassName("overlay");
 
-    content[0].children[0].innerHTML = datanews[index][0];
-    content[0].children[1].innerHTML = datanews[index][1];
-    content[0].children[2].innerHTML = datanews[index][2];
+    //Obteniendo los elementos HTML dentro del contenedor overlay
+    content[0].children[0].innerHTML = datanews[index][0]; //titulo
+    content[0].children[1].innerHTML = datanews[index][1]; //descripcion
+    content[0].children[2].setAttribute("src", datanews[index][3]); //imagen
+    content[0].children[3].innerHTML = datanews[index][2]; //span contador
+
+//aumentando en 1 las visitas
+    datanews[index][2]++;
+
+    //Declarando una variable contador que obtiene 
+    //el elemento del contador de la lista
+    var contador = document.getElementById('news_' + index);
+
+    //Seteando el nuevo valor de las visitas
+    contador.innerHTML = datanews[index][2];
 
 }
