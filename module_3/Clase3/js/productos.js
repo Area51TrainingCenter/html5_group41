d9988430f051e856bc43be98207d4cbbfc1cba37
antoniocuga <antoniocuga@gmail.com>
2014-04-05 11:29:08 -0500

    Añadiendo ejemplo de each y manipulación de doom con jquery

--- module_3/Clase3/js/productos.js
+++ module_3/Clase3/js/productos.js
@@ -1,26 +1,36 @@
+var categorias =[
+    'Hogar',
+    'Limpieza',
+    'Ocio',
+    'Musica',
+    'Dormitorio',
+    'Oficina',
+    
+];
+
 var productos = [
     {id: 'tv01',
         nombre: 'Televisor 15"',
         imagen: 'images/tv01.jpg',
-        categoria: 'hogar',
+        categoria: [1,3],
         cantidad:25
     },
     {id: 'ra01',
         nombre: 'Radio',
         imagen: 'images/radio.jpg',
-        categoria: 'musica',
+        categoria: [3],
         cantidad:25
     },
     {id: 'tv02',
         nombre: 'Television 70"',
         imagen: 'images/tv02.jpg',
-        categoria: 'hogar',
+        categoria: [1,3],
         cantidad:25
     },
     {id: 'lav01',
         nombre: 'Lavadora 100Kl',
         imagen: 'images/lav01.jpg',
-        categoria: 'limpieza',
+        categoria: [1],
         cantidad:25
     }
 ];
--- module_3/Clase3/js/script.js
+++ module_3/Clase3/js/script.js
@@ -1,12 +1,32 @@
 $(document).ready(function() {
 
-    var link = $('a.ver-mas');
+    loadHTML();
 
     $('a.ver-mas').click(function() {
-        
-        
+
 
     });
 
 });
 
+function loadHTML() {
+
+    //iterando en la lista productos
+    $.each(productos, function(index, producto) {
+
+        //creando un objeto h2 para el nombre del producto
+        var nombre = document.createElement('h2');
+        
+        //obteniendo el contenedor de la lista
+        var listcontainer = $('#product-list');
+        
+        //agregando el valor nombre a el objeto h2 -> nombre
+        $(nombre).html(producto.nombre);
+        
+        //añadiendo los nombre a la lista
+        $(listcontainer).append(nombre);
+
+    });
+
+
+}
