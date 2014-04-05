504e2f03679af970f67def71e11248817aa8ec99
antoniocuga <antoniocuga@gmail.com>
2014-04-05 14:08:58 -0500

    Añadiendo validacion para mostrar campo de email

--- module_3/Clase3/js/form.js
+++ module_3/Clase3/js/form.js
@@ -1,10 +1,49 @@
 $().ready(function() {
 
+    $('.email-form').hide();
+    
     $("#register-form").validate({
         rules: {
             nombre: "required",
-            apellido: "required"           
+            apellidos: "required",
+            fecha: {
+                required: true,
+                date: true
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        }, messages: {
+            nombre: "Escribe tu nombre ca*****",
+            apellidos: "Escribe tu apellido miie*****",
+            fecha: "Ingresa una fecha correcta"
         }
     });
+
+    $('input[name=email]').keydown(function(e) {
+
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+                (e.keyCode == 65 && e.ctrlKey === true) ||
+                (e.keyCode >= 35 && e.keyCode <= 39)) {
+            return;
+        }
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+    $('input[name=recibir]').click(function() {
+
+        var checked = $(this).attr('checked');
+
+        if (checked) {
+            $('.email-form').show();
+        } else {
+            $('.email-form').hide();
+        }
+
+    });
+
 
 });
