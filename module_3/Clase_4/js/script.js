e3ebd3a9596cb8b4dc4371734750e4248f731476
antoniocuga <antoniocuga@gmail.com>
2014-04-12 10:26:07 -0500

    Añadiendo ejercicio

--- module_3/Clase_4/js/script.js
+++ module_3/Clase_4/js/script.js
@@ -1,5 +1,15 @@
 $().ready(function() {
 
+    $('#content').load('home.html');
+
+    $('.menu > ul > li > a').click(function() {
+
+        var href = $(this).attr('href');
+
+        $('#content').load(href);
+
+        event.preventDefault();
+    });
 
 
 });
