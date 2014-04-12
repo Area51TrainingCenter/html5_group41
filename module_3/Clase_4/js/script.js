7dcb8d616459258972b10cd3dade049bcd68eca9
antoniocuga <antoniocuga@gmail.com>
2014-04-12 11:00:17 -0500

    Añadiendo render con Load

--- module_3/Clase_4/js/script.js
+++ module_3/Clase_4/js/script.js
@@ -1,15 +1,13 @@
 $().ready(function() {
 
-    $('#content').load('home.html');
+    var firsthref = $('.menu > ul > li').first().find('a');
+    $('#content').load(firsthref.attr('href'));
 
     $('.menu > ul > li > a').click(function() {
-
         var href = $(this).attr('href');
-
         $('#content').slideToggle(500, function() {
             $(this).load(href).slideToggle(500);
         });
-
         event.preventDefault();
     });
 
